4ee6a7d8a876cc52f7f6ca2690a969ef714d37f4
Lucas H. Miranda <lucashenriquemiranda1@gmail.com>
2026-09-04 21:35:23 -0300

    Add files via upload

---
+++ SuporteVida.java
@@ -0,0 +1,18 @@
+public class SuporteVida {
+
+public static void verificarsuportedevida() {
+    System.out.println("Oxigênio 99%");
+    System.out.println("Pressão do módulo: Estável");
+    System.out.println("Sistemas de reciclagem de água: Operacionais");
+    System.out.println("Nível de CO₂: Ok");
+    System.out.println("Qualidade do ar: Puro");
+    System.out.println("Temperatura interna do módulo: 25°");
+    System.out.println("Nível de energia/bateria dos sistemas: Totalmente carregados.");
+
+}
+
+public static void main(String[] args){
+    verificarsuportedevida();
+
+
+} 
